5b755a61c1e6bec19e2a3dfb24f6469bf1c3327c
Rakélene Nunes <94464632+Rakelene@users.noreply.github.com>
2022-03-21 10:53:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var nome= "Guilherme"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 5
+var notaDoQuartoBimestre = 4
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem Vindo " + nome)
+console.log(notaFixada)
